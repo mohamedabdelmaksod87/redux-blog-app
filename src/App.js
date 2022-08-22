ec26919bd4734105a00a9239136f8a2a2d85f6ff
mohamedabdelmaksod87 <mohamedabdelmaksod1987@gmail.com>
2022-08-22 07:48:25 +0200

    remove hello world msg

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import AddFormPost from "./features/posts/AddFormPost";
 function App() {
   return (
     <main className="App">
-      <h1>Hello world</h1>
       <AddFormPost />
       <PostsList />
     </main>
